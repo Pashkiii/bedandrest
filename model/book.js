9fe4760109ea4e3ce95afde88dab00552658af49
Stepan <s.le@laim.bz>
2024-09-21 17:32:17 +0300

    add sender to database

--- model/book.js
+++ model/book.js
@@ -3,35 +3,62 @@ const { makeCreateBookMessage } = require('./message/index.js');
 const { sendMessage } = require('./sender/index.js');
 const { Storage } = require('./storage/index.js');
 
-async function createBook(data) {
-    try {
-        const storage = new Storage();
-        storage.setBook(data.id, data);
+function convertToPostgresTime(date) {
+    return new Date(date.getTime() + (1000 * 60 * (-(new Date()).getTimezoneOffset()))).toISOString().replace('T', ' ').replace('Z', '');
+}
 
-        const message = makeCreateBookMessage(data);
-        const sendResult = await sendMessage(data.phone, message);
-        if (sendResult.ok) {
+async function createBook(book) {
+    try {
+        if (!book.phone || !book.beginDate) {
             return;
         }
+
+        const message = makeCreateBookMessage(book);
+        console.log(message);
+
+        // const sendResult = await sendMessage(book.phone, message);
+        // if (sendResult.ok) {
+        //     return;
+        // }
+
+        const data = {
+            id,
+            create_date: convertToPostgresTime(new Date()),
+            begin_date: convertToPostgresTime(book.beginDate),
+            end_date: convertToPostgresTime(book.endDate),
+            client_id: book.clientId,
+            client_name: book.clientName,
+            apartment_id: book.apartment.id,
+            phone: book.phone,
+            first_message_sended: true,
+            second_message_sended: false,
+            data: book.data,
+        };
+
+        const storage = new Storage();
+        await storage.setBook(data);
     } catch (err) {
         log(err);
     }
 }
 
-async function updateBook(data) {
-    try {
+async function updateBook(book) {
+    const data = {
+        begin_date: convertToPostgresTime(book.beginDate),
+        end_date: convertToPostgresTime(book.endDate),
+        client_id: book.clientId,
+        client_name: book.clientName,
+        apartment_id: book.apartment.id,
+        data: book.data,
+    };
 
-    } catch (err) {
-
-    }
+    const storage = new Storage();
+    await storage.updateBook(book.id, data);
 }
 
 async function deleteBook(data) {
-    try {
-
-    } catch (err) {
-
-    }
+    const storage = new Storage();
+    await storage.deleteBook(data.id);
 }
 
 module.exports = {
--- model/storage/index.js
+++ model/storage/index.js
@@ -1,27 +1,59 @@
+const supabaseJs = require('@supabase/supabase-js');
+
+const supabase = supabaseJs.createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
+
+const tableName = 'barbook';
+
 class Storage {
     constructor() {
         this.storage = [];
     }
 
-    setBook(book) {
-        this.storage.push(book);
-    }
+    async setBook(data) {
+        try {
+            const { error, status } = await supabase
+                .from(tableName)
+                .insert(data);
 
-    updateBook(id, book) {
-        const index = this.storage.findIndex((b) => b.id === id);
+            console.log('Supabase insert row status:', status);
 
-        if (index === -1) {
-            this.storage.push(book)
-        } else {
-            this.storage[index] = book;
+            if (error) {
+                console.error('Supabase insert row return error', error);
+            }
+
+        } catch (error) {
+            console.log('Connection to Supabase error', error);
         }
     }
 
-    deleteBook(id) {
-        const index = this.storage.findIndex((b) => b.id === id);
-        if (index === -1) return;
+    async updateBook(id, data) {
+        try {
+            const { error, status } = await supabase
+                .from(tableName)
+                .update(data)
+                .eq('id', id);
 
-        this.storage.splice(index, 1);
+            console.log('Supabase update row status:', status);
+
+            if (error) {
+                console.error('Supabase update row error', error);
+            }
+        } catch (error) {
+            console.log('Connection to Supabase error', error);
+        }
+    }
+
+    async deleteBook(id) {
+        const response = await supabase
+            .from(tableName)
+            .delete()
+            .eq('id', id);
+
+        console.log('Supabase delete response status', response.status);
+
+        if (response.error) {
+            console.error('Supabase return error when delete row', response.error);
+        }
     }
 }
 
