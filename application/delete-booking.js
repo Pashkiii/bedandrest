0c4d61d4e90171b7a02fbc9275d7383344e4f31a
S-le <leontievstepan@live.ru>
2025-01-13 16:47:50 +0300

    исправит удаление бронирования из API.

--- application/delete-booking.js
+++ application/delete-booking.js
@@ -4,7 +4,7 @@ import { log } from '../model/logger/index.js';
 export async function deleteBooking(bookingId) {
 	try {
 		const deleteBookingService = new DeleteBookingService();
-		await deleteBookingService.deleteBooking({ bookingId });
+		await deleteBookingService.deleteBooking({ id: bookingId });
 	} catch (error) {
 		await log(`ERROR. DeleteBooking. ${error.name}: ${error.message}`);
 	}
